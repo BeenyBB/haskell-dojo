900a65f00cf73ff98082bab60f21f4f22c6a6ae9
BeenyBB <bej11@yahoo.com>
2023-10-18 15:53:45 +0000

    Solutions to A1 assignment by bej

--- src/Sandbox.hs
+++ src/Sandbox.hs
@@ -1,2 +1,4 @@
 module Sandbox where
+a = 1
+
 
--- src/TTT/A1.hs
+++ src/TTT/A1.hs
@@ -1,51 +1,74 @@
 module TTT.A1 where
 
 import Data.Char (toUpper)
+import Data.Data (ConstrRep(CharConstr))
 
 -- Q#01
-
-_SIZE_ = undefined
+_SIZE_ :: Int
+_SIZE_ = 3
 
 -- Q#02
-
-_DISPLAY_LOGO_ = undefined
+_DISPLAY_LOGO_ :: Bool
+_DISPLAY_LOGO_ = True
 
 -- Q#03
+convertRowIndex :: Char -> Int
+convertRowIndex x = fromEnum (toUpper x) - 65
 
-convertRowIndex = undefined
 
 -- Q#04
-
-_INVALID_MOVE_ = undefined
+_INVALID_MOVE_ :: (Int, Int)
+_INVALID_MOVE_ = (-1,-1)
 
 -- Q#05
+_SEP_ :: String
+_SEP_ = "_|_"
 
-_SEP_ = undefined
 
 -- Q#06
 
-data Square
+data Square = X | O | E deriving (Show, Eq)
 
 -- Q#07
 
-data GameState
+data GameState = Xwon | Owon | Tie | InProgress
+    deriving (Show, Eq)
 
 -- Q#08
+type Player = Square
+type Row = [Square]
+type Line = [Square]
+type Board = [Row]
+type Move = (Int, Int)
+
 
 -- Q#09
 
-getFirstPlayer = undefined
-
-getFirstPlayer_ = undefined
+getFirstPlayer :: Bool -> Player
+getFirstPlayer_ :: Bool -> Player
+getFirstPlayer a = if a == True then X
+    else O
+getFirstPlayer_ a
+    | a == True = X
+    | a == False = O
 
 -- Q#10
+showGameState :: GameState -> String
+showGameState a = case a of
+    Xwon -> "X won the game"
+    Owon -> "O won the game"
+    Tie -> "The game is a tie"
+    InProgress -> "The game is in progress"
 
-showGameState = undefined
 
 -- Q#11
-
-switchPlayer = undefined
+switchPlayer :: Player -> Player
+switchPlayer X = O
+switchPlayer O = X
+switchPlayer E = E
 
 -- Q#12
-
-showSquare = undefined
+showSquare :: Square -> String
+showSquare X = "X"
+showSquare O = "O"
+showSquare E = "E"
